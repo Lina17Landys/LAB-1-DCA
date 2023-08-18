acf08b08febfe7584610af5375074b82a731027c
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-08-18 12:23:20 -0500

    lab 1, node mio completo, trabajando en el de alguien mas

---
+++ script.js
@@ -0,0 +1,10 @@
+import toRegexRange from "./node_modules/to-regex-range/index";
+
+const source = toRegexRange('15', '95');
+//=> 1[5-9]|[2-8][0-9]|9[0-5]
+ 
+const regex = new RegExp(`^${source}$`);
+console.log(regex.test('14')); //=> false
+console.log(regex.test('50')); //=> true
+console.log(regex.test('94')); //=> true
+console.log(regex.test('96')); //=> false
